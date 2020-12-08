ace4831dd4b4b745ec7ffd6cd69855aa18dbe4d0
Stroby241 <maarten.behn@gmail.com>
2020-12-08 12:23:33 +0100

    Matrix working

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,50 @@
 #include <Arduino.h>
 
 #define rowSize 3
-int row[] = {5, 4, 14};
+int rowPins[] = {5, 4, 14};
 
 #define collumSize 3
-int column[] = {12, 15, 10};
-
+int columnPins[] = {12, 15, 10};
 
 void setup() {
 
   Serial.begin(115200);
 
   for(int i = 0; i < rowSize; i++){
-    pinMode(row[i], OUTPUT);
+    pinMode(rowPins[i], OUTPUT);
   }
   for(int i = 0; i < collumSize; i++){
-    pinMode(column[i], OUTPUT);
+    pinMode(columnPins[i], OUTPUT);
   }
 }
 
 void setAllLEDs(bool on){
 
-  if(on){
-
-    for(int i = 0; i < rowSize; i++){
-      digitalWrite(row[i], HIGH); 
-    }
-    for(int i = 0; i < collumSize; i++){
-      digitalWrite(column[i], LOW);
-    } 
-
+  for(int i = 0; i < rowSize; i++){
+    digitalWrite(rowPins[i], on); 
   }
-  else{
-
-    for(int i = 0; i < rowSize; i++){
-      digitalWrite(row[i], LOW); 
-    }
-    for(int i = 0; i < collumSize; i++){
-      digitalWrite(column[i], HIGH);
-    } 
-
-  }
+  for(int i = 0; i < collumSize; i++){
+    digitalWrite(columnPins[i], !on);
+  } 
 }
+
 void setLED(int r, int c, bool on){
-
-  if(on){
-
-    digitalWrite(r, HIGH);
-    digitalWrite(c, LOW);
-
-  }
-  else{
-
-    digitalWrite(r, LOW);
-    digitalWrite(c, HIGH);
-
-  }
+  digitalWrite(r, on);
+  digitalWrite(c, !on);
 }
-
 
 void loop() {
+  setAllLEDs (0);
+  
   for(int i = 0; i < rowSize; i++){
 
     for(int j = 0; j < collumSize; j++){
 
-      setLED(row[i], column[j], 1);
+      setLED(rowPins[i], columnPins[j], 1);
       delay(1000);
-      setLED(row[i], column[j], 0);
+      setLED(rowPins[i], columnPins[j], 0);
       delay(1000);
     }
   }
+  
 }
